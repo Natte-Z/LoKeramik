754354d71fcf86901a3055c539a5ce2656f276d9
Natte-Z <nathalie.zauels@gmail.com>
2021-01-19 20:31:13 +0000

    added more products

--- home/views.py
+++ home/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     """ A view for the index page """
 
     return render(request, 'home/index.html')
 
+
 def courses(request):
-    return render(request, 'courses/courses.html')
+    return render(request, 'home/courses.html')
