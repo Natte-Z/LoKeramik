813bfb01862d9c7a21864ed567351ebc89ea14ff
Natte-Z <nathalie.zauels@gmail.com>
2021-01-25 19:52:14 +0000

    added courses models, admin and view

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=0)
+    duration = models.CharField(max_length=15, null=True, blank=True)
+    participants = models.DecimalField(max_digits=1, decimal_places=0)
+    level = models.CharField(max_length=15, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
