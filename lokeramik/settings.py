6ce8b56f4ac6c963ebb3ded9429dc607222e5976
Natte-Z <nathalie.zauels@gmail.com>
2021-01-28 18:43:34 +0000

    further deployment with amayon webservices

--- lokeramik/settings.py
+++ lokeramik/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 if os.path.exists("env.py"):
     import env
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
